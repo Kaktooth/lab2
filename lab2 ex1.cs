544075e42ec6bd20fa22685c4d1a24e31dfed15e
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2020-03-07 19:56:57 +0200

    Add files via upload

---
+++ lab2 ex1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+       {     int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, n];
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                 
+                }
+            }
+            for (int j= 0; j < n; j++)
+            {
+                Console.WriteLine();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(array[j,i]);
+                }
+              
+            
+            }
+           
+        }
+    }
+}
---
+++ lab2 ex3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+
+                if (array[i] % 2 == 0)
+                {
+                    Array.Clear(array, i, 1);
+                    count++;
+                }
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int i= j + 1; i < n; i++)
+                {
+                    if (array[j] == 0)
+                    {
+                        int a;
+                        a = array[i];
+                        array[i] = array[j];
+                        array[j] = a;
+                    }
+
+                }
+                
+            }
+            Console.WriteLine();
+            Array.Resize(ref array, count);
+            for (int i = 0; i <array.Length; i++)
+            {
+                    Console.Write(" "+array[i]);
+            }
+
+        }
+    }
+}
