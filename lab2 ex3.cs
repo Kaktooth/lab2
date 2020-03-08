1ea48e3f16eebc43966ffd199782767437cd532f
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2020-03-08 11:44:50 +0200

    Update lab2 ex3.cs

--- lab2 ex3.cs
+++ lab2 ex3.cs
@@ -1,35 +1,29 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp17
 {
     class Program
     {
         static void Main(string[] args)
-
         {
             int n = int.Parse(Console.ReadLine());
             int[] array = new int[n];
             int count = 0;
-
             for (int i = 0; i < n; i++)
             {
-
                 array[i] = int.Parse(Console.ReadLine());
-
             }
             for (int i = 0; i < n; i++)
             {
-
                 if (array[i] % 2 == 0)
                 {
                     Array.Clear(array, i, 1);
                     count++;
                 }
             }
-
             for (int j = 0; j < n; j++)
             {
-                for (int i= j + 1; i < n; i++)
+                for (int i = j + 1; i < n; i++)
                 {
                     if (array[j] == 0)
                     {
@@ -38,17 +32,17 @@ namespace ConsoleApp17
                         array[i] = array[j];
                         array[j] = a;
                     }
-
                 }
-                
             }
             Console.WriteLine();
             Array.Resize(ref array, count);
-            for (int i = 0; i <array.Length; i++)
-            {
-                    Console.Write(" "+array[i]);
-            }
+            for (int i = 0; i < array.Length; i++)
 
+            {
+                Console.Write(" " + array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("довжина масиву"+array.Length);
         }
     }
 }
