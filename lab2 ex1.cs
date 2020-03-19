e628ee7fccce0fea1c592bfacb4f34c8680726d5
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2020-03-19 16:58:50 +0200

    Add files via upload

---
+++ lab2 ex1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+       {     int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, n];
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i] = 1;
+                    array[i, j] = int.Parse(Console.ReadLine());
+                 
+                }
+            }
+            for (int j= 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    arr[j] *= array[i, j];
+                }
+                if (arr[j] != 0)
+                {
+                    
+                    Console.WriteLine("В {0} стопчику добуток дорівнює {1}", j + 1, arr[j]);
+                }
+               
+            }
+           
+        }
+    }
+}
